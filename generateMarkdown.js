ae47d434c5c0ba3b81bd9b36b47690df63186aaf
Barry25000 <barryengler@gmail.com>
2023-06-08 15:19:45 -0500

    update to js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,3 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-// const { default: prompt } = require('inquirer/lib/ui/prompt');
-// const { default: InputPrompt } = require('inquirer/lib/prompts/input');
-// const { default: Prompt } = require('inquirer/lib/prompts/base');
-// const { default: ListPrompt } = require('inquirer/lib/prompts/list');
-
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -37,7 +30,17 @@ function getInfo (){
         messgae: 'What license do you want to use?',
         choices: ['none', 'Apache License 2.0', 'GNU General Public License', 'MIT License', 'BSD 2 -Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License', 'Creative Commons Zera v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU Lesser General Public License v2.1', 'Mozzila Public License 2.0', 'The Unlicense'],
         name: 'License'
-    }
+    },
+    {
+        type: 'Input',
+        message: 'What is the title of the project?',
+        name: 'title'    
+    },
+    {
+        type: 'Input',
+        message: 'What is the title of the project?',
+        name: 'title'    
+    },
 ])
   .then((answers) => {
     // Use user feedback for... whatever!!
