7d6843189f82d9be054e1c167d7a0e057107f51b
Barry25000 <barryengler@gmail.com>
2023-06-08 09:11:51 -0500

    Index update

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,3 +1,10 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+// const { default: prompt } = require('inquirer/lib/ui/prompt');
+// const { default: InputPrompt } = require('inquirer/lib/prompts/input');
+// const { default: Prompt } = require('inquirer/lib/prompts/base');
+// const { default: ListPrompt } = require('inquirer/lib/prompts/list');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
@@ -16,5 +23,36 @@ function generateMarkdown(data) {
 
 `;
 }
+
+function getTitle (){
+    inquirer
+  .prompt([
+    {
+    type: 'Input',
+    message: 'What is your title of the project',
+    name: 'title'    
+    },
+    {
+    type: 'list',
+                messgae: 'What license do you want to use?',
+                choices: ['none', 'Apache License 2.0', 'GNU General Public License', 'MIT License', 'BSD 2 -Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License', 'Creative Commons Zera v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU Lesser General Public License v2.1', 'Mozzila Public License 2.0', 'The Unlicense'],
+                name: 'License'
+    }
+])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
+}
+getTitle()
+
 
 module.exports = generateMarkdown;
